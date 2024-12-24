4d126def05eebd3133e3de1514902ea00a6cc55d
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2024-12-24 02:04:18 +0000

    Refactor include paths in quadnode.cpp to use geoobjects directory

--- server/quadnode.cpp
+++ server/quadnode.cpp
@@ -1,6 +1,6 @@
-#include "point.h"
-#include "segment.h"
-#include "boundary.h"
+#include "geoobjects/point.h"
+#include "geoobjects/segment.h"
+#include "geoobjects/boundary.h"
 
 #include "quadnode.h"
 
